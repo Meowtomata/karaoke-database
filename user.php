f54fd155c7d048cbb5b5e86bf4dd1b1016749adc
Meowtomata <z2047125@turing.cs.niu.edu>
2024-11-25 15:57:49 -0600

    Add ability to connect to database
    Put your username and password in a 'passwords.php' file where
    $username = your username
    $password = your password

---
+++ user.php
@@ -0,0 +1,27 @@
+<?php
+include 'password.php'
+?>
+
+
+<?php
+function setupPDO($username, $password) {
+    try {
+        $dsn = "mysql:host=courses;dbname=z2047125";
+        $pdo = new PDO($dsn, $username, $password);
+    }
+    catch (PDOexception $e) {
+        echo "Connection to database failed: ". $e->getMessage();
+        return;
+    }
+
+    // pdo will throw exceptions when error encountered
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Succesfully connected to database.";
+}
+?>
+
+
+<?php
+setupPDO($username, $password);
+?>
