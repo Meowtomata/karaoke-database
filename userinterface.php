4a8824d953249672055281e9f4aeb857254c106c
Brian Juarez <z1967922@hopper.cs.niu.edu>
2024-11-27 16:41:33 -0600

    User Interface
    updated

--- userinterface.php
+++ userinterface.php
@@ -1,3 +1,27 @@
+
+<?php
+include 'password.php'
+?>
+
+
+<?php
+function setupPDO($username, $password, $dbname) {
+    try {
+        $dsn = "mysql:host=courses;dbname=$dbname";
+        $pdo = new PDO($dsn, $username, $password);
+    }
+    catch (PDOexception $e) {
+        echo "Connection to database failed: ". $e->getMessage();
+        return;
+    }
+
+    // pdo will throw exceptions when error encountered
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Succesfully connected to database.";
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
