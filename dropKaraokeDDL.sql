82c20496e5ff470e3e5387e88fef624780684cc3
Meowtomata <z2047125@hopper.cs.niu.edu>
2024-11-27 16:34:20 -0600

    Update KaraokeDDL.sql and add two scripts to drop tables
    KaraokeDDL.sql should follow more standard naming conventions now
    
    droptableslegacy.sql is a legacy script to drop tables from a prev
    KaraokeDDL.sql script
    
    dropKaraokeDDL.sql is a script that drops the current tables of the
    KaraokeDDL.sql script

---
+++ dropKaraokeDDL.sql
@@ -0,0 +1,12 @@
+-- Drop tables if they exist to avoid conflicts when re-running the script
+DROP TABLE IF EXISTS song_data;
+DROP TABLE IF EXISTS contributor_group;
+DROP TABLE IF EXISTS priority_queue;
+DROP TABLE IF EXISTS queue_info;
+DROP TABLE IF EXISTS queue;
+DROP TABLE IF EXISTS karaoke_file;
+DROP TABLE IF EXISTS contributor;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS song;
+
